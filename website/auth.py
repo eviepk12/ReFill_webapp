4bcbe650cad30518a365445670845eabb242e502
eviepk12 <eviepk12@gmail.com>
2022-09-13 17:51:27 +0700

    new progress!

--- website/auth.py
+++ website/auth.py
@@ -5,3 +5,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
+
+@auth.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    return render_template('sign_up.html')
--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("home.html")
+
+@views.route('/main', methods=['GET'])
+def main_page():
+    return render_template("main_page.html")
