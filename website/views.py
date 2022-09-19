c0cc8fbe9107f0b1d86e6b2e0c8d2f915b5302e5
eviepk12 <eviepk12@gmail.com>
2022-09-19 20:44:57 +0700

    new progress

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from .models import User
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -6,6 +8,10 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/main', methods=['GET'])
+@views.route('/main', methods=['GET', 'POST'])
 def main_page():
     return render_template("main_page.html")
+
+@views.route("/detail", methods=['GET'])
+def detail():
+    return render_template("detail.html")
